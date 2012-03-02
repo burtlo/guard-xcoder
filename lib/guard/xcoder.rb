c06d6204319f09fbd2b3a27bc731de3fb10bc48e
Franklin Webber <franklin.webber@gmail.com>
2012-03-02 09:20:57 -0800

    Builders are not stored but instead a path to the builder

--- lib/guard/xcoder.rb
+++ lib/guard/xcoder.rb
@@ -13,7 +13,6 @@ module Guard
         :paths => [ '.' ],
       }.update(options)
  
-      @builders = {}
       @paths = @options[:paths]
       
       file_watchers = file_watchers_for_project_watchers watchers
@@ -46,10 +45,13 @@ module Guard
     #
     def run_on_change(commands)
       
-      puts "going to perform :#{@options[:actions]} for #{commands}"
-      
+      project_name, target_name, config_name = commands.first.split("//")
+      builder = Xcode.project(project_name).target(target_name).config(config_name).builder
+
+      UI.info "[Guard::Xcoder] Performing [ #{@options[:actions].join(", ")} ] for #{project_name} > #{target_name} > #{config_name}"
+
       Array(@options[:actions]).each do |operation|
-        @builders[commands.first].send(operation)
+        builder.send(operation)
       end
       
     end
@@ -62,7 +64,13 @@ module Guard
         @paths.map {|path| Xcode.find_projects path }.flatten.compact
       end
     end
-
+    
+    #
+    # @param [Array<Watcher>] watchers the existing watchers defined within the Guardfile that specify the path the project, target,
+    #   and config.
+    # @return [Array<Watcher>] a new list of watchers for every file within the matching project > target. Each watcher when matched
+    #   will return a path to the project//target//config that needs to be built.
+    # 
     def file_watchers_for_project_watchers(watchers)
 
       watchers.map do |watcher|
@@ -73,15 +81,17 @@ module Guard
             
             source_file_builder = target.config('Debug').builder
             
+            build_action = lambda { "#{project.name}//#{target.name}//Debug" }
+            
             target.sources_build_phase.build_files.map do |file|
               project_root_dir = File.join File.dirname(project.path), project.name
               source_file_path = File.join(project_root_dir,file.path).gsub("#{::Guard.listener.directory}/",'')
               
               puts "Building watcher for file: #{source_file_path}"
               
-              @builders[source_file_path] = source_file_builder
-              watcher = ::Guard::Watcher.new(source_file_path)
+              watcher = ::Guard::Watcher.new(source_file_path,build_action)
               watcher
+
             end
           end
         end.flatten.compact.uniq
